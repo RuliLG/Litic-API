5815334c98a5e40056a2cd9a5d01fb1c2cc8ec8d
Raúl López <hi@raullg.com>
2021-01-17 20:34:16 +0000

    Update api

--- index.js
+++ index.js
@@ -5,10 +5,33 @@ const port = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
 
-app.get('/invoke', (req, res) => {
-    res.send(req.body)
+const isValidUrl = (url) => {
+    try {
+        new URL(url)
+    } catch (e) {
+        return false;
+    }
+
+    return true;
+}
+
+app.post('/invoke', async (req, res) => {
+    const { url, keyword } = req.body
+    if (!isValidUrl(url)) {
+        return res.send({ error: 'Invalid URL' })
+    }
+
+    const litic = new Litic(url, { keyword })
+    await litic.test()
+    if (litic.didFail()) {
+        return res.send({ error: 'Could not finish analysis' })
+    }
+
+    res.send({
+        results: litic.getResults()
+    })
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Listening at http://localhost:${port}`)
 })
